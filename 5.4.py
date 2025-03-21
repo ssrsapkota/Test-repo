1a5467dd70f7a4aefcfb070861d4677d9235f646
ssrs <ssrsapkota@gmail.com>
2025-03-21 16:33:40 +0545

    commited

---
+++ 5.4.py
@@ -0,0 +1,10 @@
+alien_color='red'
+if alien_color=='green':
+    print("you earned 5 points")
+elif alien_color=='yellow':
+    print("you earned 10 points") 
+else:
+    print("you earned 15 points")
+
+    
+    
